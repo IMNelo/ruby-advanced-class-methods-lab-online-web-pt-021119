9ad1eae8cd18138eefed4b9725914cb214970ea2
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-20 00:04:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -67,7 +67,7 @@ class Song
 
   def self.create_from_filename(filename)
     split_filename = filename.chomp(".mp3").split(" - ")
-    song.self.new
+    song = self.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
     song.save
